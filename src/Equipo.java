6ac7fad058e8731980ee87ede80aa8a9ef7f33df
Xaviertinoco06 <xaviertinoco006@gmail.com>
2022-03-04 16:13:20 -0600

    4 commit

--- src/Equipo.java
+++ src/Equipo.java
@@ -1,3 +1,9 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -8,5 +14,112 @@
  * @author xavie
  */
 public class Equipo {
+ static ArrayList <Equipo> equipo1= new ArrayList();
+    private String nombre;
+    private int puntos;
+    private int ganadas;
+    private int perdidas;
+   private int  golafavor;
+   private int golcontra;
+   private int diferencia;
+  private int empates;
+   private File equipos;
+
+    public Equipo(String nombre) {
+        this.nombre = nombre;
+        puntos=0;
+        ganadas=0;
+        perdidas=0;
+        golafavor=0;
+        golcontra=0;
+        diferencia=0;
+        empates=0;
+        equipos = new File("Equipo.txt");
+        
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public int getGanadas() {
+        return ganadas;
+    }
+
+    public void setGanadas(int ganadas) {
+        this.ganadas = ganadas;
+    }
+
+    public int getPerdidas() {
+        return perdidas;
+    }
+
+    public void setPerdidas(int perdidas) {
+        this.perdidas = perdidas;
+    }
+
+    public int getGolafavor() {
+        return golafavor;
+    }
+
+    public void setGolafavor(int golafavor) {
+        this.golafavor = golafavor;
+    }
+
+    public int getGolcontra() {
+        return golcontra;
+    }
+
+    public void setGolcontra(int golcontra) {
+        this.golcontra = golcontra;
+    }
+
+    public int getDiferencia() {
+        return diferencia;
+    }
+
+    public void setDiferencia(int diferencia) {
+        this.diferencia = diferencia;
+    }
+
+    public File getEquipos() {
+        return equipos;
+    }
+
+    public void setEquipos(File equipos) {
+        this.equipos = equipos;
+    }
+
+    @Override
+    public String toString() {
+         return nombre + ";" + ganadas + ";" + empates + ";" + perdidas + ";" + golafavor + ";" + golcontra + ";" + diferencia + ";" + puntos + "\n";
+    }
     
+    public void AgregarEquipos(String nombre)throws IOException{
+        FileWriter escribir=new FileWriter(equipos);
+        Equipo eq=new Equipo(nombre);
+        equipo1.add(eq);
+        escribir.write(toString()+"\n");
+        escribir.flush();
+    
+    }
+    
+  
+    
+    
+   
+   
 }
--- src/Equipos.java
+++ src/Equipos.java
@@ -1,3 +1,8 @@
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -29,18 +34,25 @@ public class Equipos extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         JfCrear = new javax.swing.JFormattedTextField();
         Bcrear = new javax.swing.JButton();
+        jFrame1 = new javax.swing.JFrame();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
         jCrear = new javax.swing.JMenuItem();
+        Jmodificar = new javax.swing.JMenuItem();
+        JCargar = new javax.swing.JMenuItem();
+        JEliminar = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
         jMenu3 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
 
         jLabel1.setFont(new java.awt.Font("Georgia", 1, 24)); // NOI18N
         jLabel1.setText("Crear Equipo");
+
+        JfCrear.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JfCrearActionPerformed(evt);
+            }
+        });
 
         Bcrear.setText("crear");
         Bcrear.addActionListener(new java.awt.event.ActionListener() {
@@ -79,23 +91,20 @@ public class Equipos extends javax.swing.JFrame {
                 .addContainerGap(64, Short.MAX_VALUE))
         );
 
+        javax.swing.GroupLayout jFrame1Layout = new javax.swing.GroupLayout(jFrame1.getContentPane());
+        jFrame1.getContentPane().setLayout(jFrame1Layout);
+        jFrame1Layout.setHorizontalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        jFrame1Layout.setVerticalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jMenu1.setText("Equipo");
-
-        jMenuItem2.setText("Modificar");
-        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem2ActionPerformed(evt);
-            }
-        });
-        jMenu1.add(jMenuItem2);
-
-        jMenuItem3.setText("Modificar");
-        jMenu1.add(jMenuItem3);
-
-        jMenuItem4.setText("Eliminar");
-        jMenu1.add(jMenuItem4);
+        jMenu1.setText("Cargar");
 
         jCrear.setText("Crear");
         jCrear.addActionListener(new java.awt.event.ActionListener() {
@@ -104,6 +113,20 @@ public class Equipos extends javax.swing.JFrame {
             }
         });
         jMenu1.add(jCrear);
+
+        Jmodificar.setText("Modificar");
+        Jmodificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JmodificarActionPerformed(evt);
+            }
+        });
+        jMenu1.add(Jmodificar);
+
+        JCargar.setText("Cargar");
+        jMenu1.add(JCargar);
+
+        JEliminar.setText("Eliminar");
+        jMenu1.add(JEliminar);
 
         jMenuBar1.add(jMenu1);
 
@@ -133,9 +156,11 @@ public class Equipos extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem2ActionPerformed
+    private void JmodificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JmodificarActionPerformed
+       jFrame1.setLocationRelativeTo(this);
+        jFrame1.setVisible(true);
+        jFrame1.setAlwaysOnTop(true);
+    }//GEN-LAST:event_JmodificarActionPerformed
 
     private void jCrearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCrearActionPerformed
       Crear.setLocationRelativeTo(this);
@@ -145,8 +170,20 @@ public class Equipos extends javax.swing.JFrame {
     }//GEN-LAST:event_jCrearActionPerformed
 
     private void BcrearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BcrearActionPerformed
-        // TODO add your handling code here:
+      Equipo eq=new Equipo(JfCrear.getText());
+      
+      try{
+          eq.AgregarEquipos(JfCrear.getText());
+          
+      }catch(IOException x){
+          Logger.getLogger(Equipos.class.getName()).log(Level.SEVERE, null, x);
+          
+      }
     }//GEN-LAST:event_BcrearActionPerformed
+
+    private void JfCrearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JfCrearActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_JfCrearActionPerformed
 
     /**
      * @param args the command line arguments
@@ -186,16 +223,17 @@ public class Equipos extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton Bcrear;
     private javax.swing.JFrame Crear;
+    private javax.swing.JMenuItem JCargar;
+    private javax.swing.JMenuItem JEliminar;
     private javax.swing.JFormattedTextField JfCrear;
+    private javax.swing.JMenuItem Jmodificar;
     private javax.swing.JMenuItem jCrear;
+    private javax.swing.JFrame jFrame1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuItem jMenuItem4;
     // End of variables declaration//GEN-END:variables
 }
